ff75f8c316f119c60164e75bf6cdebc2b5e24ae1
AnhLong <phamhoanglong1910@gmail.com>
2025-04-28 23:05:42 +0700

    Version FE

--- src/app/pages/layout/product/product.component.ts
+++ src/app/pages/layout/product/product.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../../../services/product.service';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  products: any[] = [];
 
+  constructor(private productService: ProductService) {} // Inject ProductService
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe({
+      next: (data) => {
+        console.log('Products from API:', data);
+        this.products = data;
+      },
+      error: (err) => {
+        console.error('Error fetching products:', err);
+      }
+    });
+  }
 }
