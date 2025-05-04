2c4171a4e1e268505b9951bfbfe87a9784df727b
AnhLong <phamhoanglong1910@gmail.com>
2025-05-04 22:29:09 +0700

    pussh FE1

--- src/app/pages/layout/product-detail/product-detail.component.ts
+++ src/app/pages/layout/product-detail/product-detail.component.ts
@@ -3,7 +3,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -17,8 +16,7 @@ import { ProductService } from './services/product.service';
   styleUrl: './product-detail.component.scss'
 })
 
-export class ProductDetailComponent implements OnInit {
-  private productService = inject(ProductService);
+export class ProductDetailComponent  {
   private route = inject(ActivatedRoute);
 
   product: any;
@@ -31,23 +29,6 @@ export class ProductDetailComponent implements OnInit {
   showSizeGuide: boolean = false;
 
 
-  ngOnInit(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id') || 1);
-
-    this.productService.getProduct(id).subscribe(data => {
-      this.product = data;
-      this.selectedImage = this.product.thumbnail;
-
-      // Gán mặc định color/size đầu tiên nếu có
-      this.selectedColor = data.color?.[0] || '';
-      this.selectedSize = data.size?.[0] || '';
-    });
-
-    // Cần move đoạn gọi relatedProducts vào trong subscribe
-    this.productService.getRelatedProducts().subscribe(all => {
-      this.relatedProducts = all.filter(p => p.category === this.product?.category && p.id !== id);
-    });
-  }
 
   toggleSizeGuide(event: Event) {
     event.preventDefault();
